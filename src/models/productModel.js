fd1616344e1447bc63d65f80ebdf614096665592
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-09-27 19:58:09 +0700

    add vercel.json & add engine in package.json

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -5,7 +5,7 @@ const selectAllProducts = (search, sort, limit, offset) => {
 	SELECT product.*, seller.store_name,  seller.name AS seller_name, seller.role 
 	FROM product
 	JOIN seller ON product.seller_id = seller.id 
-	WHERE product.name LIKE '%${search}%'
+	WHERE product.name ILIKE '%${search}%'
 	ORDER BY product.name ${sort}
 	LIMIT ${limit}
 	OFFSET ${offset}; 
