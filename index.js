58fa2bc42cd3e3ff5a344111c95cd44ebee5b377
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-09-27 20:18:52 +0700

    add endpoint /api

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(xss());
 app.use(cookieParser());
 
 // routers
-app.use(allRouters);
+app.use("/api", allRouters);
 
 app.listen(PORT, () => {
 	console.log(`Server berjalan di PORT: ${PORT}`);
